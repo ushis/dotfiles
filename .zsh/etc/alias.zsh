a718fa0af59290d55033e8dff81c760da9c82045
ushi <ushi@honkgong.info>
2015-01-06 16:19:14 +0100

    Add alias.zsh

---
+++ .zsh/etc/alias.zsh
@@ -0,0 +1,38 @@
+# ~/.zsh/etc/alias.zsh
+
+# Modified commands
+alias ls='ls -A --color=auto'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias diff='colordiff'
+alias df='df -h'
+alias du='du -c -h'
+alias mkdir='mkdir -p -v'
+alias ping='ping -c 3'
+alias tree='tree -aAC --dirsfirst'
+
+# Shortcuts
+alias rs='rails s'
+alias rc='rails c'
+
+# Util
+alias get='curl -#RLOC -'
+alias ssh-tunnel='ssh -ND 127.0.0.1:6666'
+
+# Navigation
+alias - -='cd -'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+
+# Disable globbing
+alias calc='noglob calc'
+alias rake='noglob rake'
+alias rsync='noglob rsync'
+
+# Systemd
+if type systemctl &>/dev/null; then
+  alias reboot='sudo systemctl reboot'
+  alias poweroff='sudo systemctl poweroff'
+fi
--- .zshrc
+++ .zshrc
@@ -6,34 +6,3 @@ source "${HOME}/.zsh/init.zsh"
 # Some vars
 export EDITOR=vim
 export TERM='xterm-256color'
-
-# Modified commands
-alias ls='ls -A --color=auto'
-alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias diff='colordiff'
-alias df='df -h'
-alias du='du -c -h'
-alias mkdir='mkdir -p -v'
-alias ping='ping -c 3'
-alias tree='tree -aAC --dirsfirst'
-
-# Util
-alias get='curl -#RLOC -'
-alias ssh-tunnel='ssh -ND 127.0.0.1:6666'
-
-# Navigation
-alias - -='cd -'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-
-# Disable globbing
-alias calc='noglob calc'
-alias rake='noglob rake'
-alias rsync='noglob rsync'
-
-# Shut down
-alias reboot='sudo systemctl reboot'
-alias poweroff='sudo systemctl poweroff'
